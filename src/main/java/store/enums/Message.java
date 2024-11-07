ba4d46a842cce101b7d450389affe04bfb2dea65
soohalee <suha630@naver.com>
2024-11-08 02:44:59 +0900

    fix(Message): 처음 안내 메시지 수정

--- src/main/java/store/enums/Message.java
+++ src/main/java/store/enums/Message.java
@@ -2,5 +2,5 @@ package store.enums;
 
 public class Message {
     public static final String WELCOME = "안녕하세요. W편의점입니다.\n"
-            + "현재 보유하고 있는 상품입니다.";
+            + "현재 보유하고 있는 상품입니다.\n";
 }
