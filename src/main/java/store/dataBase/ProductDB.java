1d2fc7b4a967ce7f38c234a76f22469420253abd
soohalee <suha630@naver.com>
2024-11-07 20:35:54 +0900

    fix(ProductDB): product.md 파일 읽기 실패시 익센션 위치 변경

--- src/main/java/store/dataBase/ProductDB.java
+++ src/main/java/store/dataBase/ProductDB.java
@@ -1,21 +1,21 @@
-package store.repository;
+package store.dataBase;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 public class ProductDB {
 
-    public List<String> findProduct() throws IOException {
+    public List<String> findProduct() {
         while (true) {
             try {
                 List<String> input = Files.readAllLines(Paths.get("./src/main/resources/products.md"));
                 input.remove(0);
                 return input;
             } catch (IOException e) {
-                throw new IOException("파일 읽기에 실패하였습니다." ,e);
+                System.out.println("파일 읽기에 실패하였습니다.");
+                e.printStackTrace();
             }
         }
     }
