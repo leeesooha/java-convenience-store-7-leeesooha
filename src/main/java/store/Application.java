fa9950aa279702060b65b5314fa6d9be4be9f7be
soohalee <suha630@naver.com>
2024-11-07 20:51:55 +0900

    fix(Application): 필요없는 try catch 제거

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -4,16 +4,14 @@ import java.io.IOException;
 import store.controller.StoreController;
 import store.service.StoreService;
 import store.view.InputView;
+import store.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
         StoreService storeService = new StoreService();
-        StoreController store = new StoreController(inputView, storeService);
-        try {
-            store.run();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        StoreController store = new StoreController(inputView, outputView, storeService);
+        store.run();
     }
 }
