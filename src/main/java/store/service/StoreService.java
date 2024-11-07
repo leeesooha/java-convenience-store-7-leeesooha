e124ab01a29c0434519018d9fbeae6ba15fc6878
soohalee <suha630@naver.com>
2024-11-07 20:38:15 +0900

    refactor(StoreService): createInventory함수 분리

--- src/main/java/store/service/StoreService.java
+++ src/main/java/store/service/StoreService.java
@@ -1,25 +1,44 @@
 package store.service;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
+import store.dataBase.ProductDB;
+import store.dataBase.PromotionDB;
 import store.model.Product;
 import store.model.Inventory;
+import store.model.Promotion;
+import store.model.PromotionCatalog;
 
 public class StoreService {
+    PromotionDB promotionDB;
+    ProductDB productDB;
 
+    public StoreService() {
+        this.promotionDB = new PromotionDB();
+        this.productDB = new ProductDB();
+    }
 
-    public Inventory addItemToInventory(List<String> input) {
+    public Inventory createInventory() {
+        List<String> products = productDB.findProduct();
         Inventory inventory = new Inventory();
 
-        for (String oneProduct : input) {
-            List<String> productInfo = Arrays.asList(oneProduct.split(","));
-            String name = productInfo.get(0);
-            int price = Integer.parseInt(productInfo.get(1));
-            int quantity = Integer.parseInt(productInfo.get(2));
-            String promotion = productInfo.get(3);
-            inventory.addProduct(new Product(name, price, quantity, promotion));
-        }
+        products.stream()
+                .map(this::toProduct)
+                .forEach(inventory::addProduct);
+
         return inventory;
+    }
+
+    private Product toProduct(String productData) {
+        List<String> productInfo = Arrays.asList(productData.split(","));
+        String name = productInfo.get(0);
+        int price = Integer.parseInt(productInfo.get(1));
+        int quantity = Integer.parseInt(productInfo.get(2));
+        String promotion = productInfo.get(3);
+        return new Product(name, price, quantity, promotion);
+    }
+
     public PromotionCatalog createPromotion() {
         PromotionCatalog promotionCatalog = new PromotionCatalog();
         List<String> promotions = promotionDB.findPromotion();
