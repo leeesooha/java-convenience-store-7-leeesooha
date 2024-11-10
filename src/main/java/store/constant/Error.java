5ec887de41f410af1b063248c6ad7f39bb6e0836
soohalee <suha630@naver.com>
2024-11-11 02:44:08 +0900

    feat(InputViewm, Error): 빈 입력일떄 에러 문구 출력하는 기능 추가

--- src/main/java/store/constant/Error.java
+++ src/main/java/store/constant/Error.java
@@ -5,7 +5,8 @@ public enum Error {
     INSUFFICIENT_STOCK_MESSAGE("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요."),
     NONE_EXIST_STOCK_MESSAGE("[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요."),
     PURCHASE_FORMAT_MESSAGE("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요."),
-    FILE_ERROR("파일 읽기에 실패하였습니다.");
+    FILE_ERROR("[ERROR] 파일 읽기에 실패하였습니다."),
+    EMPTY_INPUT("[ERROR] 빈 입력입니다.");
 
     private final String errorMessage;
 
--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -3,22 +3,35 @@ package store.view;
 import java.util.Arrays;
 import java.util.List;
 import camp.nextstep.edu.missionutils.Console;
+import store.constant.Error;
 
 public class InputView {
     private final String YES = "Y";
+    private final String NO = "N";
     private final String DELIMITER = ",";
+    private final OutputView outputView = new OutputView();
+    private final String NEW_LINE = "\n";
 
     public List<String> inputProduct() {
         List<String> input = Arrays.asList(Console.readLine().split(DELIMITER));
+
+        if (input.size() == 1 && input.get(0).isBlank()) {
+            throw new IllegalArgumentException(Error.EMPTY_INPUT.getErrorMessage());
+        }
 
         return input;
     }
 
     public boolean inputYesOrNo() {
-        String input = Console.readLine();
-        if (input.equals(YES)) {
-            return true;
+        while (true) {
+            String input = Console.readLine();
+            if (input.equals(YES)) {
+                return true;
+            }
+            if (input.equals(NO)) {
+                return false;
+            }
+            outputView.display(Error.PURCHASE_FORMAT_MESSAGE.getErrorMessage() + NEW_LINE);
         }
-        return false;
     }
 }
