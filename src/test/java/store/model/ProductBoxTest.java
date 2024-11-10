7d6d3dd7a8c7ef756831542eb9821853f8eede65
soohalee <suha630@naver.com>
2024-11-11 06:10:54 +0900

    Test(ProductBoxTest, PromotionTest): 사용하지 않는 import 삭제

--- src/test/java/store/model/ProductBoxTest.java
+++ src/test/java/store/model/ProductBoxTest.java
@@ -1,6 +1,5 @@
 package store.model;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
--- src/test/java/store/model/PromotionTest.java
+++ src/test/java/store/model/PromotionTest.java
@@ -1,7 +1,5 @@
 package store.model;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.setAllowComparingPrivateFields;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
